7eab8165d6fe019d1567bf18b4d8bce757453ed1
kira <kira.eva@protonmail.com>
2020-12-29 21:40:42 +0300

    implemented user password checking

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,46 @@
-import { Document, model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import { Document, HookNextFunction, model, Schema } from "mongoose";
 
 export interface IUser extends Document {
     username: string,
-    password: string
+    password: string,
+    comparePassword(password): Promise<boolean>
 }
 
-const userSchema = new Schema({
+const User = new Schema({
     username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
     characters: [{ type: Schema.Types.ObjectId, ref: 'Characters' }]
 })
 
-export default model<IUser>('Users', userSchema)
+
+
+User.pre('save', async function (next: HookNextFunction) {
+    const thisUser = this as IUser
+
+    if (!this.isModified('password')) return next()
+
+    try {
+        const salt = await bcrypt.genSalt(10)
+        thisUser.password = await bcrypt.hash(thisUser.password, salt)
+        return next();
+    }
+    catch (error) {
+        return next(error)
+    }
+})
+
+User.methods.comparePassword = function (this: IUser, password: string) {
+    return bcrypt.compare(password, this.password)
+}
+
+
+User.set('toJSON', {
+    transform: (doc: any, ret: any) => {
+        delete ret.password;
+        return ret;
+    }
+})
+
+
+export default model<IUser>('Users', User)
