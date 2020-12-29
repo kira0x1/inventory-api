f06cbea655126fabba072ae8aa05c0c9c11b82cf
kira <kira.eva@protonmail.com>
2020-12-29 21:41:05 +0300

    implemented jwt

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -1,10 +1,12 @@
+import * as commontags from 'common-tags';
 import { Router } from "express";
+import { sign } from 'jsonwebtoken';
+import { auth, secret } from "../../config/passport";
 import User, { IUser } from "../../models/User";
-import * as commontags from 'common-tags'
 
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
     User.find({}, (err, users) => {
         if (err) {
             return res.status(500).send({ err });
@@ -13,7 +15,32 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', async (req, res) => {
+router.post('/token', (req, res) => {
+    const { username, password } = req.body
+    const fieldsNotFound = checkForFields(username, password)
+
+    if (fieldsNotFound)
+        res.status(400).send({
+            err: commontags.commaListsAnd`
+Required fields not found: ${fieldsNotFound}`
+        });
+
+    User.findOne({ username: username }, async (err: string, user: IUser) => {
+        if (err) return res.status(400).send(err)
+        if (!user) return res.status(400).send({ error: `Cannot find user "${username}"` })
+
+        const passCheck = await user.comparePassword(password)
+
+        if (!passCheck)
+            return res.status(401).send({ err: 'Invalid Password' })
+
+        const payload = { id: user._id }
+        const token = sign(payload, secret)
+        return res.send(token)
+    })
+})
+
+router.get('/:id', auth, async (req, res) => {
     const { id } = req.params
 
     try {
@@ -27,16 +54,12 @@ router.get('/:id', async (req, res) => {
 router.post('/', (req, res) => {
     const { username, password } = req.body;
 
-    if (!username || !password) {
-        const fieldsNotFound = [];
-        if (!username) fieldsNotFound.push('username');
-        if (!password) fieldsNotFound.push('password');
-
-        return res.status(400).send({
+    const fieldsNotFound = checkForFields(username, password)
+    if (fieldsNotFound)
+        res.status(400).send({
             err: commontags.commaListsAnd`
-        Required fields not found: ${fieldsNotFound}`
+    Required fields not found: ${fieldsNotFound}`
         });
-    }
 
     const newUser = new User({
         username: username,
@@ -52,4 +75,21 @@ router.post('/', (req, res) => {
     });
 });
 
+router.get('/current', auth, (req, res) => {
+    return res.send(req.user)
+})
+
+function checkForFields(username: string, password: string) {
+    if (!username || !password) {
+        const fieldsNotFound: string[] = [];
+
+        if (!username) fieldsNotFound.push('username');
+        if (!password) fieldsNotFound.push('password');
+
+        return fieldsNotFound
+    }
+    return undefined
+}
+
 export default router;
+
